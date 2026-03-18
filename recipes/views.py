d0e25777621d8693928c3ea545ee823f866473e9
Ruben S. Melo <pinguim6970@gmail.com>
2026-03-17 21:32:26 -0300

    Various corrections to the templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+        ).order_by('-id')
     return render(
         request,
         'recipes/pages/home.html',
         context={
-            'recipes': [make_recipe() for _ in range(9)],
+            'recipes': recipes,
+        }
+    )
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+        ).order_by('-id')
+    return render(
+        request,
+        'recipes/pages/category.html',
+        context={
+            'recipes': recipes,
+            'title': f'{recipes.first().category.name} | Category | '
         }
     )
 
