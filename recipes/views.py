103362716243adae631668feadfe11bb1c8817b0
Ruben S. Melo <pinguim6970@gmail.com>
2026-03-18 08:48:17 -0300

    Correction of category and recipe views and rendering of recipes in HTML

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
-
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -17,26 +17,32 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
         is_published=True
-        ).order_by('-id')
+        ).order_by('-id'))
+    
     return render(
         request,
         'recipes/pages/category.html',
         context={
             'recipes': recipes,
-            'title': f'{recipes.first().category.name} | Category | '
+            'title': f'{recipes[0].category.name} - Category | '
         }
     )
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True
+        ).order_by('-id').first()
+
     return render(
         request,
         'recipes/pages/recipe-view.html',
         context={
-            'recipe': make_recipe(),
+            'recipe': recipe,
             'is_detail_page': True,
         }
     )
