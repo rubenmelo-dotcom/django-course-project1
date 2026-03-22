4054f0af0fc01e69cea22ac7d52cbe816b773726
Ruben S. Melo <pinguim6970@gmail.com>
2026-03-21 21:17:15 -0300

    Finishing our tests

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from .test_base_class import RecipeTestBase
+from .test_base_class import RecipeTestBase, Recipe
 from parameterized import parameterized
 
 
@@ -7,6 +7,23 @@ class RecipeModelTest(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newuser'),
+            title='title',
+            description='description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='preparation_steps',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
     
     # def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
     #     self.recipe.title = 'A' * 70
@@ -21,6 +38,27 @@ class RecipeModelTest(RecipeTestBase):
             ('servings_unit', 65),
     ])
     def test_recipe_fields_max_length(self, field, max_length):
-        setattr(self.recipe, field, 'A' * (max_length + 0))
+        setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+    
+    def test_recipe_preparation_steps_is_html_is_false_by_defaul(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.preparation_steps_is_html
+        )
+
+    def test_recipe_is_published_is_false_by_defaul(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.is_published
+        )
+    
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), needed,
+                         msg=f'Recipe string representation must be "{needed}" '\
+                         f'but "{str(self.recipe)}" was received.')
