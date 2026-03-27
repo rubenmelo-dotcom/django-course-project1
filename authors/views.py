fe957483ecdde0ccc67d62126db5ec3b2f0c090b
Ruben S. Melo <pinguim6970@gmail.com>
2026-03-27 08:46:29 -0300

    Sending POST to another URL using session and adding CSS to the form

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import Http404
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data')
+    form = RegisterForm(register_form_data)
 
     context = {
         'form': form,
@@ -18,3 +17,13 @@ def register_view(request):
         context,
     )
 
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
